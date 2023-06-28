178663839e6e1ac071adb5c3d05cdc27baf1fe80
David Perdigón <david.perdigon@nemak.com>
2023-06-28 15:50:47 -0400

    Se actualiza procedimiento almacenado para obtener errores

--- Generales/SQL/Procedimientos/GetAgilePointErrors.sql
+++ Generales/SQL/Procedimientos/GetAgilePointErrors.sql
@@ -10,38 +10,56 @@ GO
 -- Description:	<Description,,>
 -- =============================================
 CREATE PROCEDURE [dbo].[GetAgilePointErrors]
-	-- Add the parameters for the stored procedure here
-	@AppName nvarchar(100),
-	@WorkFlowID int,
-	@res nvarchar(1000) OUTPUT
-	AS
+	@AppName NVARCHAR(100),
+	@WorkFlowID INT,
+	@res NVARCHAR(1000) OUTPUT
+AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-declare @errormessage nvarchar(50);
-declare @value1 nvarchar(12);
-declare @value2 nvarchar(10);
-declare @value3 nvarchar(10);
+    DECLARE @errormessage NVARCHAR(50);
+    DECLARE @value1 NVARCHAR(12);
+    DECLARE @value2 NVARCHAR(10);
+    DECLARE @value3 NVARCHAR(10);
 
-declare errorlog_cursor CURSOR FOR
-SELECT ErrorMessage__u, Value1__u, Value2__u, Value3__u
-FROM [AP_Data_Entities_DB].[dbo].[AgilePointErrorLogs__u]
-WHERE Application__u = @AppName AND WorkFlowID__u = @WorkFlowID
+    DECLARE errorlog_cursor CURSOR FOR
+    SELECT 
+        ErrorMessage__u, 
+        Value1__u, 
+        Value2__u, 
+        Value3__u
+    FROM [dbo].[AgilePointErrorLogs__u]
+    WHERE Application__u = @AppName 
+    AND WorkFlowID__u = @WorkFlowID
 
-OPEN errorlog_cursor 
+    SET @res = '';
+    SET @res = @res + '<table>';
+    SET @res = @res + '     <thead>';
+    SET @res = @res + '         <tr>';
+    SET @res = @res + '             <th>Mensaje</th>';
+    SET @res = @res + '             <th>&nbsp;</th>';
+    SET @res = @res + '             <th>&nbsp;</th>';
+    SET @res = @res + '             <th>&nbsp;</th>';
+    SET @res = @res + '         </tr>';
+    SET @res = @res + '     </thead>';
+    SET @res = @res + '     <tbody>';
+    
+    OPEN errorlog_cursor 
+    FETCH NEXT FROM errorlog_cursor INTO @errorMessage, @value1, @value2, @value3
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        SET @res = @res + '     <tr>';
+        SET @res = @res + '         <td>' + @errormessage + '</td>';
+        SET @res = @res + '         <td>' +  @value1 + '</td>';
+        SET @res = @res + '         <td>' +  @value2 + '</td>';
+        SET @res = @res + '         <td>' +  @value3 + '</td>';
+        SET @res = @res + '     </tr>';
 
-FETCH NEXT FROM errorlog_cursor INTO @errorMessage,@value1,@value2,@value3
-SET @res='';
-WHILE @@FETCH_STATUS = 0
-BEGIN
-	SET @res = @res + LEFT(@errormessage + SPACE(50),50) + ':' + LEFT(@value1 + SPACE(12),12) + ':' + @value2 + ':' + @value3 + Char(13)
-	FETCH NEXT FROM errorlog_cursor INTO @errorMessage,@value1,@value2,@value3
-END
-CLOSE errorlog_cursor;
-DEALLOCATE errorlog_cursor;
-
-
+        FETCH NEXT FROM errorlog_cursor INTO @errorMessage, @value1, @value2, @value3
+    END
+    CLOSE errorlog_cursor;
+    DEALLOCATE errorlog_cursor;
+    SET @res = @res + '     </tbody>';
+    SET @res = @res + '</table>';
 END
 GO
