b55d07f6cb33f7be20c9d02bd5fa08a2d2837635
Pavel Medina <pavel.medina@nemak.com>
2023-06-30 08:51:01 -0600

    Get Agile Point Errors
    Se agregaron 6 valores nuevos para identificar errores

--- Generales/SQL/Procedimientos/GetAgilePointErrors.sql
+++ Generales/SQL/Procedimientos/GetAgilePointErrors.sql
@@ -1,5 +1,6 @@
-
 DROP PROCEDURE [dbo].[GetAgilePointErrors]
+GO
+/****** Object:  StoredProcedure [dbo].[GetAgilePointErrors]    Script Date: 30/06/2023 12:38:58 a. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -16,6 +17,12 @@ CREATE PROCEDURE [dbo].[GetAgilePointErrors]
     @TitleValue2 NVARCHAR(100) NULL,
     @TitleValue3 NVARCHAR(100) NULL,
     @TitleValue4 NVARCHAR(100) NULL,
+	@TitleValue5 NVARCHAR(100) NULL,
+    @TitleValue6 NVARCHAR(100) NULL,
+    @TitleValue7 NVARCHAR(100) NULL,
+    @TitleValue8 NVARCHAR(100) NULL,
+	@TitleValue9 NVARCHAR(100) NULL,
+    @TitleValue10 NVARCHAR(100) NULL,
 	@res VARCHAR(MAX) OUTPUT
 AS
 BEGIN
@@ -26,6 +33,12 @@ BEGIN
     DECLARE @value2 NVARCHAR(100);
     DECLARE @value3 NVARCHAR(100);
     DECLARE @value4 NVARCHAR(100);
+	DECLARE @value5 NVARCHAR(100);
+    DECLARE @value6 NVARCHAR(100);
+    DECLARE @value7 NVARCHAR(100);
+    DECLARE @value8 NVARCHAR(100);
+	DECLARE @value9 NVARCHAR(100);
+    DECLARE @value10 NVARCHAR(100);
 
     DECLARE errorlog_cursor CURSOR FOR
     SELECT 
@@ -33,7 +46,13 @@ BEGIN
         Value1__u, 
         Value2__u, 
         Value3__u,
-        Value4__u
+        Value4__u,
+		Value5__u, 
+        Value6__u, 
+        Value7__u,
+        Value8__u,
+		Value9__u, 
+        Value10__u
     FROM [dbo].[AgilePointErrorLogs__u]
     WHERE Application__u = @AppName 
     AND WorkFlowID__u = @WorkFlowID
@@ -58,6 +77,30 @@ BEGIN
     IF ISNULL(@TitleValue4, '') <> '' BEGIN
         SET @res = @res + '             <th style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;color: #96979D;background-color: #F9F9F9;border-top: 3px solid #0072c6 !important;">' + @TitleValue4 + '</th>';
     END
+	    
+    IF ISNULL(@TitleValue5, '') <> '' BEGIN
+        SET @res = @res + '             <th style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;color: #96979D;background-color: #F9F9F9;border-top: 3px solid #0072c6 !important;">' + @TitleValue5 + '</th>';
+    END
+	    
+    IF ISNULL(@TitleValue6, '') <> '' BEGIN
+        SET @res = @res + '             <th style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;color: #96979D;background-color: #F9F9F9;border-top: 3px solid #0072c6 !important;">' + @TitleValue6 + '</th>';
+    END
+	    
+    IF ISNULL(@TitleValue7, '') <> '' BEGIN
+        SET @res = @res + '             <th style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;color: #96979D;background-color: #F9F9F9;border-top: 3px solid #0072c6 !important;">' + @TitleValue7 + '</th>';
+    END
+	    
+    IF ISNULL(@TitleValue8, '') <> '' BEGIN
+        SET @res = @res + '             <th style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;color: #96979D;background-color: #F9F9F9;border-top: 3px solid #0072c6 !important;">' + @TitleValue8 + '</th>';
+    END
+	    
+    IF ISNULL(@TitleValue9, '') <> '' BEGIN
+        SET @res = @res + '             <th style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;color: #96979D;background-color: #F9F9F9;border-top: 3px solid #0072c6 !important;">' + @TitleValue9 + '</th>';
+    END
+	    
+    IF ISNULL(@TitleValue10, '') <> '' BEGIN
+        SET @res = @res + '             <th style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;color: #96979D;background-color: #F9F9F9;border-top: 3px solid #0072c6 !important;">' + @TitleValue10 + '</th>';
+    END
 
     SET @res = @res + '             <th style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;color: #96979D;background-color: #F9F9F9;border-top: 3px solid #0072c6 !important;">Mensaje</th>';
     SET @res = @res + '         </tr>';
@@ -65,7 +108,7 @@ BEGIN
     SET @res = @res + '     <tbody>';
     
     OPEN errorlog_cursor 
-    FETCH NEXT FROM errorlog_cursor INTO @errorMessage, @value1, @value2, @value3, @value4
+    FETCH NEXT FROM errorlog_cursor INTO @errorMessage, @value1, @value2, @value3, @value4, @value5, @value6, @value7, @value8, @value9, @value10
     WHILE @@FETCH_STATUS = 0
     BEGIN
         SET @res = @res + '     <tr>';
@@ -85,15 +128,38 @@ BEGIN
         IF ISNULL(@TitleValue4, '') <> '' BEGIN
             SET @res = @res + '         <td style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;">' +  @value4 + '</td>';
         END
+		
+        IF ISNULL(@TitleValue5, '') <> '' BEGIN
+            SET @res = @res + '         <td style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;">' +  @value5 + '</td>';
+        END
+		
+        IF ISNULL(@TitleValue6, '') <> '' BEGIN
+            SET @res = @res + '         <td style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;">' +  @value6 + '</td>';
+        END
+		
+        IF ISNULL(@TitleValue7, '') <> '' BEGIN
+            SET @res = @res + '         <td style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;">' +  @value7 + '</td>';
+        END
+		
+        IF ISNULL(@TitleValue8, '') <> '' BEGIN
+            SET @res = @res + '         <td style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;">' +  @value8 + '</td>';
+        END
+		
+        IF ISNULL(@TitleValue9, '') <> '' BEGIN
+            SET @res = @res + '         <td style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;">' +  @value9 + '</td>';
+        END
+		
+        IF ISNULL(@TitleValue10, '') <> '' BEGIN
+            SET @res = @res + '         <td style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;">' +  @value10 + '</td>';
+        END
 
         SET @res = @res + '         <td style="padding: 8px;text-align: left;border: 1px solid #DDDDDD;">' + @errormessage + '</td>';
         SET @res = @res + '     </tr>';
 
-        FETCH NEXT FROM errorlog_cursor INTO @errorMessage, @value1, @value2, @value3, @value4
+        FETCH NEXT FROM errorlog_cursor INTO @errorMessage, @value1, @value2, @value3, @value4, @value5, @value6, @value7, @value8, @value9, @value10
     END
     CLOSE errorlog_cursor;
     DEALLOCATE errorlog_cursor;
     SET @res = @res + '     </tbody>';
     SET @res = @res + '</table>';
 END
-GO
