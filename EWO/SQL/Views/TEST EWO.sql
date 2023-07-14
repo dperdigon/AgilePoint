92990254a2cb185fd60b3823624d6bced52d7fb4
David Perdigón <davidperdigon.g@hotmail.com>
2023-07-14 11:24:10 -0400

    Se suben cambios en vistas del reporte de EWO

--- EWO/SQL/Views/TEST EWO.sql
+++ EWO/SQL/Views/TEST EWO.sql
@@ -18,17 +18,18 @@ SELECT
 	B.EMAIL_ADDRESS,
 	B.TaskLevel,
 	CASE
+		WHEN B.STATUS = 'Removed' THEN 'Skipped'
 		WHEN B.TaskLevel = 'Requestor' AND B.STATUS = 'Completed' THEN 'Send'
-		WHEN B.TaskLevel = 'Requestor' AND B.STATUS = 'Cancelled' THEN 'Exit'
-		WHEN B.TaskLevel = 'Manager' AND B.STATUS = 'Completed' AND B2.DISPLAY_NAME = 'Engineer' THEN 'Approve'
-		WHEN B.TaskLevel = 'Manager' AND B.STATUS = 'Completed' AND B2.DISPLAY_NAME = 'Requestor' THEN 'Return form to requestor'
-		WHEN B.TaskLevel = 'Manager' AND B.STATUS = 'Cancelled' THEN 'Reject'
-		WHEN B.TaskLevel = 'Engineer' AND B.STATUS = 'Completed' AND B2.DISPLAY_NAME = 'Requestor' THEN 'Approve'
-		WHEN B.TaskLevel = 'Engineer' AND B.STATUS = 'Completed' AND B2.DISPLAY_NAME = 'Manager' THEN 'Reject'
-		WHEN B.TaskLevel = 'Approver' AND B.STATUS = 'Completed' AND B2.DISPLAY_NAME IS NULL THEN 'Approve'
-		WHEN B.TaskLevel = 'Approver' AND B.STATUS = 'Completed' AND B2.DISPLAY_NAME = 'Engineer' THEN 'Reject'
-		WHEN B2.DISPLAY_NAME IS NULL AND IIF(B.STATUS = 'Overdue', 'Assigned', IIF(B.STATUS = 'New', 'Assigned', B.STATUS)) = 'Assigned' THEN 'In Progress'
-		WHEN B2.DISPLAY_NAME IS NULL AND B.STATUS = 'Cancelled' THEN 'WF APP'
+		WHEN B.TaskLevel = 'Requestor' AND (B.STATUS = 'Cancelled' OR (B.STATUS = 'Completed' AND B2.TaskLevel IS NULL)) THEN 'Exit'
+		WHEN B.TaskLevel = 'Manager' AND B.STATUS = 'Completed' AND B2.TaskLevel = 'Engineer' THEN 'Approve'
+		WHEN B.TaskLevel = 'Manager' AND B.STATUS = 'Completed' AND B2.TaskLevel = 'Requestor' THEN 'Return form to requestor'
+		WHEN B.TaskLevel = 'Manager' AND (B.STATUS = 'Cancelled' OR (B.STATUS = 'Completed' AND B2.TaskLevel IS NULL)) THEN 'Reject'
+		WHEN B.TaskLevel = 'Engineer' AND B.STATUS = 'Completed' AND (B2.TaskLevel = 'Approver' OR B2.TaskLevel IS NULL) THEN 'Approve'
+		WHEN B.TaskLevel = 'Engineer' AND B.STATUS = 'Completed' AND B2.TaskLevel = 'Manager' THEN 'Reject'
+		WHEN B.TaskLevel = 'Approver' AND B.STATUS = 'Completed' AND B2.TaskLevel IS NULL THEN 'Approve'
+		WHEN B.TaskLevel = 'Approver' AND B.STATUS = 'Completed' AND B2.TaskLevel = 'Engineer' THEN 'Reject'
+		WHEN B2.TaskLevel IS NULL AND IIF(B.STATUS = 'Overdue', 'Assigned', IIF(B.STATUS = 'New', 'Assigned', B.STATUS)) = 'Assigned' THEN 'In Progress'
+		WHEN B2.TaskLevel IS NULL AND B.STATUS = 'Cancelled' THEN 'WF APP'
 		ELSE ''
 	END 'Button Pressed',
 	B.STATUS,
@@ -37,6 +38,5 @@ SELECT
 	A.ProcessID__u
 FROM EWOFormData__u A
 LEFT JOIN vw_TasksWithOrderForEwo B ON B.PROC_INST_ID = A.ProcessID__u
-LEFT JOIN vw_TasksWithOrderForEwo B2 ON B2.PROC_INST_ID = A.ProcessID__u AND B2.TaskNum = (B.TaskNum + 1)
+LEFT JOIN vw_TasksWithOrderForEwo B2 ON B2.PROC_INST_ID = A.ProcessID__u AND B2.TaskNum = (B.TaskNum + 1) AND B2.ApproverNum = 1
 LEFT JOIN GBLComment__u C ON C.EwoRequest__u = A.DocumentID__u AND LOWER(C.CREATED_BY) = LOWER(B.EMAIL_ADDRESS)
-WHERE B.[STATUS] <> 'Removed'
